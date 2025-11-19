10d347911f18f3b31e146b56785e0d33ec9fd5a4
TheMightyRoman <RomanKuznyuk@gmail.com>
2025-11-19 20:00:57 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Drawing2D;
+using System.Drawing.Drawing2D; //16
 using System.Windows.Forms;
 
 namespace LabWork16
